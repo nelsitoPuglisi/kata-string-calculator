b0e5129e03114bf0d0cd8ea86bfeae9764017437
nelsito <nelson.puglisi@protonmail.com>
2020-08-06 11:35:39 -0300

    StringCalculator should add two numbers separated by space and comma: Green

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -4,7 +4,7 @@ class StringCalculator {
         return if (numbers.isEmpty())
             0
         else {
-            val integers = numbers.split(",").map { it.toInt() }
+            val integers = numbers.split(",").map { it.trim().toInt() }
             integers.sum()
         }
     }
