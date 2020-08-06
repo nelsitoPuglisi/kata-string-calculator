4c403fdee88e35689216239220d4d8a124544752
nelsito <nelson.puglisi@protonmail.com>
2020-08-06 12:01:25 -0300

    refactor: Extract number conversion

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -1,12 +1,19 @@
+import java.lang.Exception
+import java.lang.NumberFormatException
+
 class StringCalculator {
 
     fun add(numbers: String): Int {
         return if (numbers.isEmpty())
             0
         else {
-            val integers = numbers.split(",").map { it.trim().toInt() }
+            val integers = numbers.split(",").map { toNumber(it) }
             integers.sum()
         }
+    }
+
+    private fun toNumber(it: String): Int {
+        return it.trim().toInt()
     }
 
 }
