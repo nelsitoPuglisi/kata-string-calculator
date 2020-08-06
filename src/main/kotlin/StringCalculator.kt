a752cbb2e99a3ec96db582215626f1582279341c
nelsito <nelson.puglisi@protonmail.com>
2020-08-06 12:46:24 -0300

    refactor: DefaultSplitter

--- src/main/kotlin/StringCalculator.kt
+++ src/main/kotlin/StringCalculator.kt
@@ -7,13 +7,19 @@ class StringCalculator {
         return if (numbers.isEmpty())
             0
         else {
-            val integers = numbers.split(",", "\n").map { toNumber(it) }
-            integers.sum()
+            DefaultSplitter(numbers).sum()
         }
+    }
+
+}
+
+class DefaultSplitter(private val numbers: String) {
+
+    fun sum(): Int {
+        return numbers.split(*mutableListOf(",", "\n").toTypedArray()).map { toNumber(it) }.sum()
     }
 
     private fun toNumber(it: String): Int {
         return it.trim().toInt()
     }
-
 }
